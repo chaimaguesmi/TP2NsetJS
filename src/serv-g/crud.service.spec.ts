087d7c0645d10bb0d50144612fed4bc6bc939625
Chaima Guesmi <chaima.guesmi@insat.uccar.tn>
2024-11-28 21:02:11 +0100

    service generique for users

--- src/serv-g/crud.service.spec.ts
+++ src/serv-g/crud.service.spec.ts
@@ -2,14 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { CrudService } from './crud.service';
 
 describe('CrudService', () => {
-  let service: CrudService;
+  let service: CrudService<any>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [CrudService],
     }).compile();
 
-    service = module.get<CrudService>(CrudService);
+    service = module.get<CrudService<any>>(CrudService);
   });
 
   it('should be defined', () => {
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import { CrudService } from 'src/serv-g/crud.service';
 
 @Injectable()
-export class UserService {
+export class UserService extends CrudService<User>{
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
-  ) {}
-  async create(createUserDto: CreateUserDto): Promise<User> {
+  ) {
+    super(userRepository);
+
+  }
+  /*async create(createUserDto: CreateUserDto): Promise<User> {
     const user = this.userRepository.create(createUserDto);
     return await this.userRepository.save(user);
   }
@@ -44,5 +46,5 @@ export class UserService {
     }
 
     await this.userRepository.remove(user);
-  }
+  }*/
 }
